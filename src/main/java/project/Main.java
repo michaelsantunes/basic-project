8015b3f74b7bb9b01abd854f978e1cf1b19db04c
Michael da Silva Antunes <CTW02109@criticaltechworks.com>
2022-04-19 14:22:41 +0100

    Initial commit

---
+++ src/main/java/project/Main.java
@@ -0,0 +1,12 @@
+package project;
+
+public class Main {
+
+    public int sumValue(int v1, int v2) {
+        return v1 + v2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().sumValue(1, 45));
+    }
+}
